6b8a9c894c138b2c1d5b9fced3054685766588bb
futuravis <futuravis@github.com>
2018-05-06 20:09:12 +0530

    Initial Commit

---
+++ ACalulator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ACalulator
+//
+//  Created by Sivaramakrishnan on 06/05/18.
+//  Copyright © 2018 Sivaramakrishnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ACalulator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ACalulator
+//
+//  Created by Sivaramakrishnan on 06/05/18.
+//  Copyright © 2018 Sivaramakrishnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
